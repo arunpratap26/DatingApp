736efd8873f0527a0e2654909956c1a8a1fa6355
arunpratap26 <arunpratap26@yahoo.com>
2024-12-14 02:42:54 +0530

    End of section 12

--- API/Controllers/AccountController.cs
+++ API/Controllers/AccountController.cs
@@ -4,32 +4,33 @@ using API.Data;
 using API.DTOs;
 using API.Entities;
 using API.Interfaces;
+using AutoMapper;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 
 namespace API.Controllers
 {
-    public class AccountController(DataContext context, ITokenService tokenService) : BaseApiController
+    public class AccountController(DataContext context, ITokenService tokenService, IMapper mapper) : BaseApiController
     {
         [HttpPost("register")]
         public async Task<ActionResult<UserDto>> Register(RegisterDto registerDto)
         {
             if (await userExists(registerDto.Username)) return BadRequest("Username is taken");
-            return Ok();
-            // using var hmac = new HMACSHA512();
-            // var user = new AppUser
-            // {
-            //     UserName = registerDto.Username.ToLower(),
-            //     PasswordHash = hmac.ComputeHash(Encoding.UTF8.GetBytes(registerDto.Password)),
-            //     PasswordSalt = hmac.Key
-            // };
-            // context.Users.Add(user);
-            // await context.SaveChangesAsync();
-            // return new UserDto
-            // {
-            //     Username = user.UserName,
-            //     Token = tokenService.CreateToken(user)
-            // };
+          
+            using var hmac = new HMACSHA512();
+            var user = mapper.Map<AppUser>(registerDto);
+            user.UserName = registerDto.Username.ToLower();
+            user.PasswordHash =hmac.ComputeHash(Encoding.UTF8.GetBytes(registerDto.Password));
+            user.PasswordSalt =hmac.Key;
+
+            context.Users.Add(user);
+            await context.SaveChangesAsync();
+            return new UserDto
+            {
+                Username = user.UserName,
+                KnownAs =user.KnownAs,
+                Token = tokenService.CreateToken(user)
+            };
         }
 
         [HttpPost("login")]
@@ -53,6 +54,7 @@ namespace API.Controllers
             return new UserDto
             {
                 Username = user.UserName,
+                KnownAs =user.KnownAs,
                 Token = tokenService.CreateToken(user),
                 PhotoUrl = user.Photos.FirstOrDefault(x => x.IsMain)?.Url
             };
--- client/src/app/members/photo-editor/photo-editor.component.ts
+++ client/src/app/members/photo-editor/photo-editor.component.ts
@@ -10,76 +10,89 @@ import { Photo } from '../../_models/Photo';
 @Component({
   selector: 'app-photo-editor',
   standalone: true,
-  imports: [NgClass,NgFor,NgIf,FileUploadModule,DecimalPipe,NgStyle],
+  imports: [NgClass, NgFor, NgIf, FileUploadModule, DecimalPipe, NgStyle],
   templateUrl: './photo-editor.component.html',
   styleUrl: './photo-editor.component.css'
 })
-export class PhotoEditorComponent implements OnInit{
+export class PhotoEditorComponent implements OnInit {
   private accountService = inject(AccountService);
   private memberService = inject(MembersService);
-   member = input.required<Member>();
-   uploader?:FileUploader;
-   hasBaseDropZoneOver =false;
-   baseUrl = environment.apiUrl;
-   memberChange = output<Member>();
+  member = input.required<Member>();
+  uploader?: FileUploader;
+  hasBaseDropZoneOver = false;
+  baseUrl = environment.apiUrl;
+  memberChange = output<Member>();
 
-   ngOnInit(): void {
-     this.initializeUploader();
-   }
+  ngOnInit(): void {
+    this.initializeUploader();
+  }
 
-   fileOverBase(e: any){
-    this,this.hasBaseDropZoneOver =e;
-   }
+  fileOverBase(e: any) {
+    this, this.hasBaseDropZoneOver = e;
+  }
 
-deletePhoto(photo: Photo){
-  this.memberService.deletePhoto(photo).subscribe({
-    next:_=>{
-      const updatedMember ={...this.member()};
-      updatedMember.photos = updatedMember.photos.filter(x=>x.id!==photo.id);
-      this.memberChange.emit(updatedMember);
-    }
-  })
-}
+  deletePhoto(photo: Photo) {
+    this.memberService.deletePhoto(photo).subscribe({
+      next: _ => {
+        const updatedMember = { ...this.member() };
+        updatedMember.photos = updatedMember.photos.filter(x => x.id !== photo.id);
+        this.memberChange.emit(updatedMember);
+      }
+    })
+  }
 
-   setMainPhoto(photo : Photo){
+  setMainPhoto(photo: Photo) {
     this.memberService.setMainPhoto(photo).subscribe({
-      next:_=>{
+      next: _ => {
         const user = this.accountService.currentUser();
-        if(user){
-          user.photoUrl =photo.url;
+        if (user) {
+          user.photoUrl = photo.url;
           this.accountService.setCurrentUser(user);
         }
-        const updatedMember ={...this.member()}
-        updatedMember.photoUrl =photo.url;
-        updatedMember.photos.forEach(p=> {
-          if(p.isMain) p.isMain =false;
-          if(p.id===photo.id) p.isMain =true;
+        const updatedMember = { ...this.member() }
+        updatedMember.photoUrl = photo.url;
+        updatedMember.photos.forEach(p => {
+          if (p.isMain) p.isMain = false;
+          if (p.id === photo.id) p.isMain = true;
         });
         this.memberChange.emit(updatedMember);
       }
     })
-   }
+  }
 
-   initializeUploader(){
+  initializeUploader() {
     this.uploader = new FileUploader({
-      url:this.baseUrl+ 'users/add-photo',
-      authToken: 'Bearer '+this.accountService.currentUser()?.token,
-      isHTML5:true,
+      url: this.baseUrl + 'users/add-photo',
+      authToken: 'Bearer ' + this.accountService.currentUser()?.token,
+      isHTML5: true,
       allowedFileType: ['image'],
-      removeAfterUpload:true,
-      autoUpload:false,
+      removeAfterUpload: true,
+      autoUpload: false,
       maxFileSize: 10 * 1024 * 1024,
     });
 
-    this.uploader.onAfterAddingFile =(file) =>{
-      file.withCredentials =false
+    this.uploader.onAfterAddingFile = (file) => {
+      file.withCredentials = false
     }
 
-    this.uploader.onSuccessItem =(item,response,status,header) =>{
+    this.uploader.onSuccessItem = (item, response, status, header) => {
       const photo = JSON.parse(response);
-      const updatedMember ={...this.member()};
+      const updatedMember = { ...this.member() };
       updatedMember.photos.push(photo);
       this.memberChange.emit(updatedMember);
+      if (photo.isMain) {
+        const user = this.accountService.currentUser();
+        if (user) {
+          user.photoUrl = photo.url;
+          this.accountService.setCurrentUser(user)
+        }
+        updatedMember.photoUrl = photo.url;
+        updatedMember.photos.forEach(p => {
+          if (p.isMain) p.isMain = false;
+          if (p.id === photo.id) p.isMain = true;
+        });
+        this.memberChange.emit(updatedMember);
+      }
     }
-   }
+  }
 }
